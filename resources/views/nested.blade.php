12506ff409e3076e8851ce18a0643ef018698ddb
RaihanIIUC <51045712+RaihanIIUC@users.noreply.github.com>
2022-02-06 22:44:41 +0600

    add

---
+++ resources/views/nested.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <div class="container items-center justify-center">
+        <section class="py-24 py-lg-36 bg-white"
+            style="background-image: url('pstls-assets/images/forms/form-1-shadow.png'); background-repeat: no-repeat; background-size: cover; z-index: -1;">
+            <div class="container">
+                <div class="text-center mw-sm mx-auto px-3">
+                    <h2 class="mb-2 lh-sm mt-6 font-heading">Log in to your account</h2>
+                    <p class="mb-6 lh-lg">Please enter your details to proceed.</p>
+                    <form class="text-start" action="">
+                        <label class="d-block mb-6">
+                            <span class="small">Parent GO </span>
+                            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">Book</option>
+                                <option value="2">Car</option>
+                                <option value="3">Electornics</option>
+                        
+                            </select>
+                        </label>
+
+                        <label class="d-block mb-4">
+                            <span class="small">Child G1 Name </span>
+                            <input class="form-control mt-2" type="text">
+                        </label>
+
+                        <label class="d-block mb-6">
+                            <span class="small">Generation 1 </span>
+                            <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                                
+                            </select> 
+                        </label>
+
+                        
+                        
+                        <button class="btn btn-dark d-block mb-4 btn-lg" type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
